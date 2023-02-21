4e6f69e5d33f2eebd95138d52e52170a5eb26cf6
felipemarqs <felipe.marqs@outlook.com>
2023-02-20 22:49:21 -0300

    feat: :zap: Database Created
    Connection with Docker and Prostgres created.

--- src/index.js
+++ src/index.js
@@ -13,5 +13,5 @@ app.use(express.json())
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`😜 Server is listening on: http://localhost/${port}`);
+  console.log(`😜 Server is listening on: http://localhost:${port}`);
 });
