379c9c8a74e6583db07e7b8413ddd953d9f584ce
Felipe Marques <felipe.marqs@outlook.com>
2023-03-14 14:49:40 +0000

    Init 😀: Project Initialized

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,11 +1,25 @@
-import GlobalStyles from "./assets/styles/global";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<GlobalStyles>
-			<h1> My Contats</h1>
-		</GlobalStyles>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
