2e66ca9ab2237a937d57086c0140ebf3021eaa61
felipemarqs <felipe.marqs@outlook.com>
2023-03-15 20:14:15 -0300

    feat: att

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,47 +1,60 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import FormGroup from "../FormGroup"
-import Input from "../Input/input"
-import Select from "../Select/Select"
-import Button from '../Button/Button'
-import { Form, ButtonContainer } from "./styles"
-
+import FormGroup from "../FormGroup";
+import Input from "../Input/input";
+import Select from "../Select/Select";
+import Button from "../Button/Button";
+import { Form, ButtonContainer } from "./styles";
 
 const ContactForm = ({ buttonLabel }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [category, setCategory] = useState("");
 
-    const [name, setName] = useState('')
+  return (
+    <Form>
+      <FormGroup>
+        <Input
+          value={name}
+          placeholder="Nome"
+          onChange={(event) => setName(event.target.value)}
+        />
+      </FormGroup>
 
-    return (
-        <Form>
-            <FormGroup>
-                <Input
-                    value={name}
-                    placeholder="Nome"
-                    onChange={(event) => setName(event.target.value)}
+      <FormGroup error="O formato do E-mail é inválido!">
+        <Input
+          value={email}
+          placeholder="Email"
+          error
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </FormGroup>
 
-                />
-            </FormGroup>
-            <FormGroup
-                error="O formato do E-mail é inválido!"
-            >
-                <Input placeholder="Email" error />
-            </FormGroup>
-            <FormGroup>
-                <Input placeholder="Telefone" />
-            </FormGroup>
+      <FormGroup>
+        <Input
+          value={phone}
+          placeholder="Telefone"
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
 
+      <FormGroup>
+        <Select
+        value={category}
+        onChange={(event) => setCategory(event.target.value)}
+        >
+            
+            <option value="">Categoria</option>
+            <option value="Instagram">Instagram</option>
+            <option value="discord">Discord</option>
+        </Select>
+      </FormGroup>
 
-            <FormGroup>
-                <Select>
-                    <option value="Instagram">INSTAGRAM</option>
-                </Select>
-            </FormGroup>
-
-            <ButtonContainer>
-                <Button type="submit">{buttonLabel}</Button>
-            </ButtonContainer>
-
-        </Form>
-    )
-}
-export default ContactForm
+      <ButtonContainer>
+        <Button type="submit">{buttonLabel}</Button>
+      </ButtonContainer>
+    </Form>
+  );
+};
+export default ContactForm;
