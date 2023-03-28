9828581b69b493928924cfd054023e7ec2e463cf
Felipe Marques <felipe.marqs@outlook.com>
2023-03-28 18:15:12 +0000

    modified:   fe/src/pages/Home/styles.js

--- fe/src/pages/Home/styles.js
+++ fe/src/pages/Home/styles.js
@@ -152,3 +152,28 @@ export const ErrorContainer = styled.div`
     }
   }
 `;
+
+export const EmptyListContainer = styled.div`
+  margin-top: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 30%;
+   
+  }
+
+  p {
+    color: ${({ theme }) => theme.colors.gray[200]};
+    text-align: center;
+    margin-top: 8px;
+  }
+
+  strong {
+    color: ${({ theme }) => theme.colors.primary[500]};
+  }
+
+
+`;
