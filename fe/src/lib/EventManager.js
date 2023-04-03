b41a7e5c1ff3034de041949ab1d423bdce604605
Felipe Marques <felipe.marqs@outlook.com>
2023-04-03 18:26:15 +0000

    feat: :sparkles: EventManager Refatored

--- fe/src/lib/EventManager.js
+++ fe/src/lib/EventManager.js
@@ -1,38 +1,33 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {};
+    this.listeners = new Map();
   }
 
   //Criando o método para ser emitido
   on(event, listener) {
-    //Recebe o evento e a função que será execurada
 
-    //Se o evento não existir na propriedade listener, settamos ela como um array vazio
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, [])
     }
 
-    //Faz um push no array criado acima para armazenar a função que será executada
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   //Método para emitir o evento
   emit(event, payload) {
-    //Recebe o evento e o payload, as informações que serão utilizadas pela função.
 
-    //Se o evento passado não existir no atributo do objeto, simplesmente retorna o código
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
 
-    //Vai no atributo do objeto com o evento passado e faz uma iteração no array passando as informações do payload para cara função que esteja dentro do array.
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
   }
 
   removeListener(event, listenerToRemove) {
-    const listeners = this.listeners[event];
+    const listeners = this.listeners.get(event);
 
     if (!listeners) {
       return;
@@ -42,31 +37,9 @@ export default class EventManager {
       (listener) => listener !== listenerToRemove
     );
 
-    this.listeners[event] = filteredListeners
+    this.listeners.set(event, filteredListeners)
   }
 }
 
 const toastEventManager = new EventManager();
-
-const addToast1 = (payload) => {
-   console.log('tost 1 add', payload) 
-}
-
-const addToast2 = (payload) => {
-    console.log('tost 2 add', payload) 
- }
-
-toastEventManager.on("addtoast", addToast1);
-toastEventManager.on("addtoast", addToast2);
-
-console.log("primeiro",toastEventManager.listeners)
-
-
-toastEventManager.emit("addtoast", { type: "default", text: "default text" });
-
-
-toastEventManager.removeListener('addtoast', addToast1);
-
-console.log("segundo",toastEventManager)
-
 
